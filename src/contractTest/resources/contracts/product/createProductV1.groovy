1a945c07265b3211645c008109bd14685c7f4837
tzappiello <tzappiello@gmail.com>
2026-05-06 10:17:55 -0300

    Update createProductV1 contract to include test and stub value matchers

--- src/contractTest/resources/contracts/product/createProductV1.groovy
+++ src/contractTest/resources/contracts/product/createProductV1.groovy
@@ -11,13 +11,34 @@ Contract.make {
         }
         urlPath("/api/v1/products") {
             body([
-                    name        : "Notebook X11",
-                    brand       : "Deep Driver",
-                    regularPrice: 1500.00,
-                    salePrice   : 1200.00,
-                    enabled     : true,
-                    categoryId  : "7f1a3b2c-9d8e-4a5b-8c7d-6e5f4a3b2c1d",
-                    description : 'A Gamer Notebook'
+                    name        : value(
+                            test("Notebook X11"),
+                            stub(nonBlank())
+                    ),
+                    brand       : value(
+                            test("Deep Driver"),
+                            stub(nonBlank())
+                    ),
+                    regularPrice: value(
+                            test(1500.00),
+                            stub(number())
+                    ),
+                    salePrice   : value(
+                            test(1200.00),
+                            stub(number())
+                    ),
+                    enabled     : value(
+                            test(false),
+                            stub(anyBoolean())
+                    ),
+                    categoryId  : value(
+                            test("7f1a3b2c-9d8e-4a5b-8c7d-6e5f4a3b2c1d"),
+                            stub(anyUuid())
+                    ),
+                    description : value(
+                            test("A Gamer Notebook"),
+                            stub(optional(nonBlank()))
+                    )
             ])
         }
     }
