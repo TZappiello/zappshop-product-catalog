514ecc0576b88638d6c9314e3af9157327ff6ca8
tzappiello <tzappiello@gmail.com>
2026-05-08 16:27:52 -0300

    Add CategoryMinimalOutput DTO to Product Catalog project

---
+++ src/main/java/com/zappshop/product/catalog/presentation/CategoryMinimalOutput.java
@@ -0,0 +1,18 @@
+package com.zappshop.product.catalog.presentation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryMinimalOutput {
+
+    private UUID id;
+    private String name;
+}
