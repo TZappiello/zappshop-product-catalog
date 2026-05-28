74619644092d4d780c5546057c2b876499605b2d
tzappiello <tzappiello@gmail.com>
2026-05-28 14:40:29 -0300

    Add CategoryQueryService interface to Product Catalog project

---
+++ src/main/java/com/zappshop/product/catalog/application/product/query/CategoryQueryService.java
@@ -0,0 +1,8 @@
+package com.zappshop.product.catalog.application.product.query;
+
+import java.util.UUID;
+
+public interface CategoryQueryService {
+    CategoryDetailOutput findById(UUID categoryId);
+    PageModel<CategoryDetailOutput> filter(Integer size, Integer number);
+}
