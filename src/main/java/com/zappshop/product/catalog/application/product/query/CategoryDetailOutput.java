c5c520e6ab78227fa7e92b7d0de3fed0283b6161
tzappiello <tzappiello@gmail.com>
2026-05-29 15:55:51 -0300

    Add CategoryDetailOutput class to Product Catalog project

---
+++ src/main/java/com/zappshop/product/catalog/application/product/query/CategoryDetailOutput.java
@@ -0,0 +1,19 @@
+package com.zappshop.product.catalog.application.product.query;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDetailOutput {
+    private UUID id;
+    private String name;
+    private Boolean enabled;
+
+}
