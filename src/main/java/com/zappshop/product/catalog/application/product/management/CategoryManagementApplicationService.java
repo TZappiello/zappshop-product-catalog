36e041759b70e270346bd3e1564a250d1d5ff3ca
tzappiello <tzappiello@gmail.com>
2026-06-01 09:47:08 -0300

    Add CategoryManagementApplicationService to Product Catalog project

---
+++ src/main/java/com/zappshop/product/catalog/application/product/management/CategoryManagementApplicationService.java
@@ -0,0 +1,19 @@
+package com.zappshop.product.catalog.application.product.management;
+
+import java.util.UUID;
+
+public class CategoryManagementApplicationService {
+
+    public UUID create(CategoryInput input){
+        return null;
+    }
+
+    public void update(UUID categoryId, CategoryInput input){
+
+    }
+
+    public void disable(UUID categoryId){
+
+    }
+
+}
